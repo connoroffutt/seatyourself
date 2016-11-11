d8421159668d0ae6cb00a888acf1d0db57411087
nicky shieh <nicky.y.shieh@gmail.com>
2016-11-11 15:59:02 -0500

    Created restaurant controllers

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -1,2 +1,16 @@
 class RestaurantController < ApplicationController
+
+  def index
+    @restaurant = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :location)
+  end
 end
