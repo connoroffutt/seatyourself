98c9e97413adcb821f07d51ff04b21a1bcda1078
Andrea <andrea.africa93@gmail.com>
2016-11-12 16:45:27 -0500

    Implemented log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  root 'restaurant#index'
 
+  root 'restaurant#index'
 
   resources :users
   resources :restaurant
   resources :reservation
+  resources :sessions, only: [:new, :create, :destroy]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
