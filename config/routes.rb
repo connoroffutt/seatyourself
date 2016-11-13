fc4be2baefff4dce4389a5cc425e27c493ca0896
connor <connor.ferguson.offutt@gmail.com>
2016-11-13 18:00:22 -0500

    added some scss and made a few retaurants to help me see how it will layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  root 'restaurant#index'
-
 
   resources :user
   resources :restaurant
   resources :reservation
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
